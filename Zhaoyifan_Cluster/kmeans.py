b536b95962f51c0872235ad52b2bdbd3e918821e
Qiaoqian-Fan0214 <yzhao642@connect.hkust-gz.edu.cn>
2025-05-08 01:23:35 +0800

    初步搞了一下两种聚类方法，基于密度的聚类虽然聚类失败，但也可以写在报告中作为实验

--- Zhaoyifan_Cluster/kmeans.py
+++ Zhaoyifan_Cluster/kmeans.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import OPTICS
+from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-from sklearn.cluster import DBSCAN
 
-
+# 读取数据
 df = pd.read_csv('/Users/qiaoqian./Desktop/ML_Project/preprocessing/processed_data_label_encoding.csv')  # 请替换为你的绝对路径
-X = df.iloc[:, 1:17].values
-y_true = df.iloc[:, -1].values
+X = df.iloc[:, 1:17].values  # 特征
+y_true = df.iloc[:, -1].values  # 原始标签
 
-# === 3. OPTICS 聚类 ===
-optics = OPTICS(min_samples=5, xi=0.1, min_cluster_size=0.05)
-optics.fit(X)
-labels = optics.labels_
+# === 3. K-means 聚类 ===
+kmeans = KMeans(n_clusters=7, random_state=42)  # 可以根据需要选择不同的簇数
+labels = kmeans.fit_predict(X)  # 获取聚类标签
 
 # 打印每个类别的数量
 unique_labels, counts = np.unique(labels, return_counts=True)
@@ -23,14 +21,9 @@ unique_labels, counts = np.unique(labels, return_counts=True)
 for label, count in zip(unique_labels, counts):
     print(f"Cluster {label}: {count} points")
 
-
-
-
-
 # === 4. t-SNE 降维 ===
 tsne = TSNE(n_components=2, perplexity=30, random_state=42)
 X_tsne = tsne.fit_transform(X)
-
 
 # === 5. 可视化聚类结果 ===
 plt.figure(figsize=(10, 5))
@@ -38,14 +31,14 @@ plt.figure(figsize=(10, 5))
 # 聚类结果
 plt.subplot(1, 2, 1)
 plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=labels, cmap='tab10', s=20)
-plt.title('OPTICS Cluste（t-SNE）')
+plt.title('K-means Cluster (t-SNE)')
 plt.xlabel('t-SNE1')
 plt.ylabel('t-SNE2')
 
 # 原始标签（可选）
 plt.subplot(1, 2, 2)
 plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=y_true, cmap='tab10', s=20)
-plt.title('Original（t-SNE）')
+plt.title('Original (t-SNE)')
 plt.xlabel('t-SNE1')
 plt.ylabel('t-SNE2')
 
